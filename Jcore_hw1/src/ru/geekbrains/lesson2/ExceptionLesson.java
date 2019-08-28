f3480469606a7a578e7d429fb513adfecd180bee
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-08-28 22:08:24 +0300

    Класс с методом преобразования строк в числа и их суммирования

--- Jcore_hw1/src/ru/geekbrains/lesson2/ExceptionLesson.java
+++ Jcore_hw1/src/ru/geekbrains/lesson2/ExceptionLesson.java
@@ -2,20 +2,41 @@ package ru.geekbrains.lesson2;
 
 public class ExceptionLesson {
 
-    public static int summator(int[][] IntArray) throws MyArraySizeException
+    public static int summator(String[][] StringArray) throws MyArraySizeException, MyArrayDataException, Exception
     {
-        //check size
-        if(IntArray.length != 4) {
-            throw new MyArraySizeException();
+        final int PREFERRED_SIZE = 4;
+        //Проверить размер полученного массива
+        if(StringArray.length != PREFERRED_SIZE) {
+            throw new MyArraySizeException("Размер массива String не равен " + PREFERRED_SIZE);
         }
-        for (int i = 0; i < 4; i++) {
-            if(IntArray[i].length != 4){
-                throw new MyArraySizeException();
+        for (int i = 0; i < PREFERRED_SIZE; i++) {
+            if(StringArray[i].length != PREFERRED_SIZE){
+                throw new MyArraySizeException("Размер массива String["+ i +"] не равен " + PREFERRED_SIZE);
             }
         }
 
+        //Преобразовать массив строк в массив целых чисел и проссумировать его элементы
+        int sum = 0;
+        int[][] IntArray = new int[PREFERRED_SIZE][PREFERRED_SIZE];
 
+        for (int i = 0; i < PREFERRED_SIZE; i++)
+            for (int j = 0; j < PREFERRED_SIZE; j++){
+                try {
+                    IntArray[i][j] = Integer.parseInt(StringArray[i][j]);
+                    sum += IntArray[i][j];
+                }
+                catch (NumberFormatException e){
+                    String message = "Не удалось преобразовать в int " +
+                            "элемент массива String из ячейки ["+i+"]["+j+"]";
+                    throw new MyArrayDataException(message);
+                }
+                catch (Exception e) {
+                    throw e;
+                }
 
-        return 0;
+            }
+        /**/
+
+        return sum;
     }
 }
