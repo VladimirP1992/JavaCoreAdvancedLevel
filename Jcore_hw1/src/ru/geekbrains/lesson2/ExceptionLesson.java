62536c1797223182918f647a77e8b7ecd58116ba
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-08-28 20:27:56 +0300

    Проверка размера массива в методе (пункт 1 ДЗ)

--- Jcore_hw1/src/ru/geekbrains/lesson2/ExceptionLesson.java
+++ Jcore_hw1/src/ru/geekbrains/lesson2/ExceptionLesson.java
@@ -2,5 +2,20 @@ package ru.geekbrains.lesson2;
 
 public class ExceptionLesson {
 
-    public static int lol = 100;
+    public static int summator(int[][] IntArray) throws MyArraySizeException
+    {
+        //check size
+        if(IntArray.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < 4; i++) {
+            if(IntArray[i].length != 4){
+                throw new MyArraySizeException();
+            }
+        }
+
+
+
+        return 0;
+    }
 }
