83f1d6e07674ca1dd48d9efb28af267a24cee461
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-08-28 22:08:58 +0300

    Класс Main с демонстрацией исключительныз ситуаций

--- Jcore_hw1/src/ru/geekbrains/jcorehw1/Main.java
+++ Jcore_hw1/src/ru/geekbrains/jcorehw1/Main.java
@@ -1,10 +1,56 @@
 package ru.geekbrains.jcorehw1;
 
+import ru.geekbrains.lesson2.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        //Создадим ситуацию с недопустимым размером массива
+        String[][] strArr = new String[3][5];
+        try {
+            ExceptionLesson.summator(strArr);
+        }
+        catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        Human human = new Human(10, 10);
+        System.out.println();
+        //Заполним массив числами в строковом представлении без ошибок
+        strArr = new String[4][4];
+        for (int i = 0; i < strArr.length; i++)
+            for(int j = 0; j< strArr[i].length; j++) {
+                strArr[i][j] = String.valueOf(i+j);
+            }
+        try {
+            int sum = ExceptionLesson.summator(strArr);
+            System.out.println("Сумма элементов массива в числовом представлении = " + sum);
+        }
+        catch (Exception e) {
+            System.out.println("Это сообщение не должно выводиться");
+        }
+
+        System.out.println();
+        //Вводим в массив недопустимый элемент
+        strArr[2][1] = "asdfsd";
+        try {
+            ExceptionLesson.summator(strArr);
+        }
+        catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
+
+/*lesson1
+Human human = new Human(10, 10);
         Cat cat = new Cat(8, 8);
         Robot robot = new Robot(5, 5);
 
@@ -28,7 +74,4 @@ public class Main {
         for (Moveable mov : competitors) {
             mov.info();
         }
-
-    }
-}
-
+ */
