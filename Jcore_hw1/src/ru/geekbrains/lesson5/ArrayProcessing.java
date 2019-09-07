332432b869b4206b70c2166fdf1bdc3436a1b450
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-09-07 13:29:13 +0300

    Метод вычисления выражения с двумя аргументами

--- Jcore_hw1/src/ru/geekbrains/lesson5/ArrayProcessing.java
+++ Jcore_hw1/src/ru/geekbrains/lesson5/ArrayProcessing.java
@@ -2,4 +2,8 @@ package ru.geekbrains.lesson5;
 
 public class ArrayProcessing {
 
+    public static float equationCalc(float floatArg, int intArg){
+        float resalt = (float)(floatArg * Math.sin(0.2f + intArg / 5) * Math.cos(0.2f + intArg / 5) * Math.cos(0.4f + intArg / 2));
+        return resalt;
+    }
 }
