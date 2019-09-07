b71655788fed1269096d2a2b126a7ec72eabc77e
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-09-07 13:36:45 +0300

    Метод перезаполнения массива значениями вычисленными по формуле (возвращает время исполнения)

--- Jcore_hw1/src/ru/geekbrains/lesson5/ArrayProcessing.java
+++ Jcore_hw1/src/ru/geekbrains/lesson5/ArrayProcessing.java
@@ -6,4 +6,15 @@ public class ArrayProcessing {
         float resalt = (float)(floatArg * Math.sin(0.2f + intArg / 5) * Math.cos(0.2f + intArg / 5) * Math.cos(0.4f + intArg / 2));
         return resalt;
     }
+
+    public  long refillArrInOneThread(float[] floatArr){
+        long begin = System.currentTimeMillis();
+
+        int arrLength = floatArr.length;
+        for (int i = 0; i < arrLength; i++){
+            floatArr[i] = equationCalc(floatArr[i], i);
+        }
+
+        return (System.currentTimeMillis() - begin);
+    }
 }
