03822aaa55c39358e2a43986e182e6545b68c437
VladimirP1992 <p.vladimir.m@yandex.ru>
2019-09-10 22:05:49 +0300

    чет запутался в гите, закинул проект в ветку 6-го урока

--- Jcore_hw1/src/ru/geekbrains/lesson6/MyServer.java
+++ Jcore_hw1/src/ru/geekbrains/lesson6/MyServer.java
@@ -1,4 +1,76 @@
 package ru.geekbrains.lesson6;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class MyServer {
+    public static void main(String[] args) {
+        Socket socket = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        Thread readThread, writeThread;
+
+        try (ServerSocket serverSocket = new ServerSocket(8189)){
+            System.out.println("Сервер запущен...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключен...");
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            readThread = new Thread(()->{
+               while(true){
+                   try {
+                       String message = in.readUTF();
+                       if (message.equalsIgnoreCase("/end")) {
+                           System.out.println("The client closed the connection");
+                           out.writeUTF("/end");
+                           break;
+                       }
+                       System.out.println("Client: " + message);
+                   }
+                   catch (IOException e){
+                       e.printStackTrace();
+                   }
+               }
+            });
+            writeThread = new Thread(()->{
+                while(true){
+                    try {
+                        Scanner keyboardIn = new Scanner(System.in);
+                        String message = keyboardIn.nextLine();
+                        out.writeUTF(message);
+                        if (message.equalsIgnoreCase("/end")) {
+                            break;
+                        }
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            readThread.start();
+            writeThread.start();
+
+            readThread.join();
+            writeThread.join();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                socket.close();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
